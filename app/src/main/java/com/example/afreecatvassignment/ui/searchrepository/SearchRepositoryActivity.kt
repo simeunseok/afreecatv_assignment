09672ca6b9c2f3f3058b651e84de85828f233992
simeunseok <ses8323@gmail.com>
2022-01-06 16:22:45 +0900

    Issue #6 feat: DataBinding 및 ViewModel 연동

--- app/src/main/java/com/example/afreecatvassignment/ui/searchrepository/SearchRepositoryActivity.kt
+++ app/src/main/java/com/example/afreecatvassignment/ui/searchrepository/SearchRepositoryActivity.kt
@@ -3,16 +3,23 @@ package com.example.afreecatvassignment.ui.searchrepository
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
 import com.example.afreecatvassignment.R
+import com.example.afreecatvassignment.databinding.ActivitySearchRepositoryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SearchRepositoryActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivitySearchRepositoryBinding
     private val viewModel by viewModels<SearchRepositoryViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search_repository)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_search_repository)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        setContentView(binding.root)
     }
 }
