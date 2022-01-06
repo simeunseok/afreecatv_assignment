03accd8ba353c6d766ebdc79ccc4d3dcc21e228c
simeunseok <ses8323@gmail.com>
2022-01-06 17:21:56 +0900

    Issue #6 feat: DiffUtil 추가

--- app/src/main/java/com/example/afreecatvassignment/ui/searchrepository/model/GitRepository.kt
+++ app/src/main/java/com/example/afreecatvassignment/ui/searchrepository/model/GitRepository.kt
@@ -1,7 +1,19 @@
 package com.example.afreecatvassignment.ui.searchrepository.model
 
+import androidx.recyclerview.widget.DiffUtil
+
 data class GitRepository(
     val name: String,
     val avatarUrl: String,
     val language: String,
-)
+) {
+    companion object {
+        val diffUtil by lazy {
+            object : DiffUtil.ItemCallback<GitRepository>() {
+                override fun areItemsTheSame(oldItem: GitRepository, newItem: GitRepository) = oldItem.name == newItem.name
+
+                override fun areContentsTheSame(oldItem: GitRepository, newItem: GitRepository) = oldItem == newItem
+            }
+        }
+    }
+}
