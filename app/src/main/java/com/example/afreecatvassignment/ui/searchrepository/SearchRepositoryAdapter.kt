33bffc9d0e0dd6e1303a659efeb0ace57b4699c3
simeunseok <ses8323@gmail.com>
2022-01-06 17:22:17 +0900

    Issue #6 feat: RecyclerView Adapter 구현

---
+++ app/src/main/java/com/example/afreecatvassignment/ui/searchrepository/SearchRepositoryAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.afreecatvassignment.ui.searchrepository
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.afreecatvassignment.R
+import com.example.afreecatvassignment.databinding.ItemRepositoryBinding
+import com.example.afreecatvassignment.ui.searchrepository.model.GitRepository
+
+class SearchRepositoryAdapter: ListAdapter<GitRepository, SearchRepositoryAdapter.SearchRepositoryViewHolder>(GitRepository.diffUtil) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchRepositoryViewHolder {
+        val binding: ItemRepositoryBinding =
+            DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.item_repository, parent, false)
+        return SearchRepositoryViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: SearchRepositoryViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class SearchRepositoryViewHolder(private val binding: ItemRepositoryBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: GitRepository) {
+            binding.item = item
+        }
+    }
+
+}
