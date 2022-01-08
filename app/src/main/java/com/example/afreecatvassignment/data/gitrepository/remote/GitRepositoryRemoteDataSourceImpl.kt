5cd09599c5fa6c7f2b0b455ad1c4e76bbc29a3d8
simeunseok <ses8323@gmail.com>
2022-01-08 20:48:19 +0900

    Issue #16 modify: fetch 함수 반환값 수정
    예외가 발생할 시 Null을 반환하도록 수정

--- app/src/main/java/com/example/afreecatvassignment/data/gitrepository/remote/GitRepositoryRemoteDataSourceImpl.kt
+++ app/src/main/java/com/example/afreecatvassignment/data/gitrepository/remote/GitRepositoryRemoteDataSourceImpl.kt
@@ -1,12 +1,13 @@
 package com.example.afreecatvassignment.data.gitrepository.remote
 
-import com.example.afreecatvassignment.data.gitrepository.ResponseGitRepositoryList
 import javax.inject.Inject
 
 class GitRepositoryRemoteDataSourceImpl @Inject constructor(
     private val gitRepositoryRemoteService: GitRepositoryRemoteService
 ) : GitRepositoryRemoteDataSource {
 
-    override suspend fun fetchGitRepositoryList(q: String, page: Int, perPage: Int) =
+    override suspend fun fetchGitRepositoryList(q: String, page: Int, perPage: Int) = kotlin.runCatching {
         gitRepositoryRemoteService.fetchGitRepositoryList(q, page, perPage)
+    }.getOrNull()
+
 }
